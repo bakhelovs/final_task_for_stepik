7d6f6cee64ba33d1e3b5487a5deb3394b785d828
Sespacecmd <sespace@ya.ru>
2020-12-13 22:01:31 +0300

    minor updates

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REG_FORM_LINK), "Reg form is not presented"
+        assert True
