738fdc2a9d8f201bdbaf25782eab93faa50e4e99
Sespacecmd <sespace@ya.ru>
2020-12-15 19:53:19 +0300

    emoved extra lines

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,14 @@
-from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .login_page import LoginPage
+from .base_page import BasePage
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    pass
+    # def __init__(self, *args, **kwargs):
+    # super(MainPage, self).__init__(*args, **kwargs)
+    # метод __init__ вызывается при создании объекта. Конструктор выше
+    # с ключевым словом super на самом деле только вызывает конструктор
+    # класса предка и передает ему все те аргументы, которые мы передали в
+    # конструктор MainPage.
