3b715d47f95f0c56307b9737c0a7bedf4386c996
Sespacecmd <sespace@ya.ru>
2020-12-16 08:29:13 +0300

    added new tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,35 @@
-from .pages.main_page import MainPage
+from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
+
 
 LINK_GLOBAL = "http://selenium1py.pythonanywhere.com/"
 
+
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = LINK_GLOBAL
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = LINK_GLOBAL
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_the_basket_is_empty()
+    basket_page.should_be_the_basket_is_empty_message()
+
+
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = LINK_GLOBAL
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
