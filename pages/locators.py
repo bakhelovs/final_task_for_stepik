7d00a4ded6f855d7cc0e3bc8b7cd8b25a70729b7
Sespacecmd <sespace@ya.ru>
2020-12-16 08:34:10 +0300

    added new locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,29 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
-class LoginPageLocators():
+class BasketPageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
+    MESSAGE_BASKET_IS_EMPTY = (By.XPATH, '//*[@id="content_inner"]/p')  # лучше заменить на корзину с товарами
+    BASKET_TOTALS_FORM = (By.CSS_SELECTOR, "basket_totals")
+
+
+class MainPageLocators:
+    pass
+
+
+class LoginPageLocators:
     REG_FORM_LINK = (By.CSS_SELECTOR, "form#register_form.well")
     LOGIN_FORM_LINK = (By.CSS_SELECTOR, "form#login_form.well")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_LINK = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
