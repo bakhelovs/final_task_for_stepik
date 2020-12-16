508b783913149b372c1a5516e9932695dc6999a2
Sespacecmd <sespace@ya.ru>
2020-12-16 08:33:28 +0300

    maked code more readeble

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-from .locators import MainPageLocators
+from .locators import BasePageLocators
 
 
 class LoginPage(BasePage):
@@ -10,13 +10,19 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        # Проверка в линке текста login
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK),\
+            "Login link is not presented"
         assert "login" in self.browser.current_url, "'login' not in current url"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login form is not presented"
+        # Проверка наличия формы с логином и паролем
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK),\
+            "Login form is not presented"
         assert True
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REG_FORM_LINK), "Reg form is not presented"
+        # Проверка наличия формы регистрации
+        assert self.is_element_present(*LoginPageLocators.REG_FORM_LINK),\
+            "Registration form is not presented"
         assert True
