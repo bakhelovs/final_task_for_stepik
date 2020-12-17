b319b7d7c14b202507cb03b46534e50a137161e4
Sespacecmd <sespace@ya.ru>
2020-12-18 02:33:49 +0300

    added new locators for register form

--- pages/locators.py
+++ pages/locators.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators:
     BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
-    MESSAGE_BASKET_IS_EMPTY = (By.XPATH, '//*[@id="content_inner"]/p')  # лучше заменить на корзину с товарами
+    MESSAGE_BASKET_IS_EMPTY = (By.XPATH, '//*[@id="content_inner"]/p')
     BASKET_TOTALS_FORM = (By.CSS_SELECTOR, "basket_totals")
 
 
@@ -19,6 +20,10 @@ class MainPageLocators:
 class LoginPageLocators:
     REG_FORM_LINK = (By.CSS_SELECTOR, "form#register_form.well")
     LOGIN_FORM_LINK = (By.CSS_SELECTOR, "form#login_form.well")
+    EMAIL_INPUT_REG_FORM = (By.ID, "id_registration-email")
+    PASS_INPUT_REG_FORM = (By.ID, "id_registration-password1")
+    PASS_CONFIRM_INPUT_REG_FORM = (By.ID, "id_registration-password2")
+    REG_BUTTON_REG_FORM = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
